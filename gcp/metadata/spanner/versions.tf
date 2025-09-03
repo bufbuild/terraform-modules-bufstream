8e1b217bd2381755d8282aebafbadfbeeb5295fc
Joey Dunst <jdunst@buf.build>
2025-09-03 13:33:34 -0500

    fixing gcp warnings

--- gcp/metadata/spanner/versions.tf
+++ gcp/metadata/spanner/versions.tf
@@ -1,13 +1,12 @@
 terraform {
   required_version = "~> 1.5.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.39.0"
     }
-
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "~> 3.7.2"
     }
   }
